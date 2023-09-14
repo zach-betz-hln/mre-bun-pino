1a7147200ad8ca9f3645234f71ae28031baa5362
Zachary Betz <zachary.betz@hln.com>
2023-09-14 16:17:51 -0500

    reproduce it

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import pino from 'pino';
+
+console.log('Hello via Bun!');
+
+const prettyLogging = process.env.PRETTY_LOGGING === 'true';
+
+const logger = pino({
+  transport: prettyLogging
+    ? {
+        target: 'pino-pretty',
+        options: {
+          colorize: true
+        }
+      }
+    : undefined
+});
+
+logger.info('Hello via Pino!');
